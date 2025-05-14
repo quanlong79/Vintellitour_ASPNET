aa24e49f67b823dc89e8a14cbe18746c3b3c2c41
Huy <giahuy171024@gmail.com>
2025-05-14 14:23:09 +0700

    huyvy_push

---
+++ Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vintellitour_Framework.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();  // Sẽ tìm Views/Main/AboutUs.cshtml
+        }
+
+        public IActionResult sharespace()
+        {
+            return View();  
+        }
+    }
+}
